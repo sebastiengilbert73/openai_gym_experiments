8b4dd7036f7997eef85824b7bcf3b772829e4313
Sébastien Gilbert <sebastiengilbert73@yahoo.ca>
2019-01-28 20:19:50 -0500

    useQLearning.py: Added epsilon

--- Blackjack-v0/useQLearning.py
+++ Blackjack-v0/useQLearning.py
@@ -18,18 +18,52 @@ class BlackjackQLearningEnvironment:
     def ObservationAsInt(self, observationTuple):
         return observationTuple[0] * 22 + observationTuple[1] * 2 + observationTuple[2]
 
+    def ObservationAsList(self, observationInt):
+        observationList = [0, 0, 0]
+        residual = observationInt
+        observationList[2] = residual % 2
+        residual -= observationList[2]
+        observationList[1] = int( (residual % 22) / 2 )
+        residual -= observationList[1] * 2
+        observationList[0] = int( residual / 22 )
+        return observationList
+
 
 def main():
     print ("Blackjack-v0/useQLearning.py main()")
     blackjackEnv = BlackjackQLearningEnvironment()
     solver = QLearning.Solver(numberOfObservations=(32 * 11 * 2),
                               numberOfActions=2,
-                              gamma=0.99,
+                              gamma=1.0,
                               learningRate=0.01,
                               environment=blackjackEnv,
-                              defaultValue=0.01
+                              defaultValue=0.01,
+                              epsilonRampDownNumberOfEpisodes = 10000,
                               )
-    solver.Solve(10000, writeToConsole=True)
+    solver.Solve(100000, writeToConsole=True)
+
+    print (solver.Q[300])
+    foundQ = solver.Q
+    outputFile = open('foundQ.csv', 'w+')
+    outputFile.write("observation, playerSum, dealerShowing, hasUsableAce, action, value\n")
+    for obs in range(foundQ.shape[0]):
+        obsList = blackjackEnv.ObservationAsList(obs)
+        for action in range(foundQ.shape[1]):
+            value = foundQ[obs, action]
+            outputFile.write("{}, {}, {}, {}, {}, {}\n".format(obs, obsList[0], obsList[1], obsList[2], action, value))
+    outputFile.close()
+
+    outputFile2 = open('suttonBarto_p121.csv', 'w+')
+    outputFile2.write("Dealer:,A,2,3,4,5,6,7,8,9,10\n")
+    for thereIsAUsableAce in [1, 0]:
+        for playerSum in range(21, 10, -1):
+            outputFile2.write("{},".format(playerSum))
+            for dealerShowing in range(1, 11):
+                observation = blackjackEnv.ObservationAsInt((playerSum, dealerShowing, thereIsAUsableAce))
+                bestAction = solver.BestAction(observation)
+                outputFile2.write("{},".format(bestAction))
+            outputFile2.write("\n")
+    outputFile2.close()
 
     # Test it a large number of times
     rewardSum = 0
